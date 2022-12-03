213411e6cf191a8bb2d00c0432d0e6d0015a8f2a
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2022-12-03 15:40:14 -0500

    made changes in added files

--- promises.js
+++ promises.js
@@ -1,16 +1,21 @@
-function onLoad(src){
-    return new Promise((resolve, reject) => {
-        script = document.createElement('script');
+function loader(src, callback){
+   
+        let script = document.createElement('script');
         script.src = src;
-        script.onload = () => resolve(script);
-        script.onerror = () => reject(new Error('this script link is not owrking yo'));
+        script.onload = () => callback(script);
+        // script.onerror = () => reject(new Error('this script link is not owrking yo'));
         document.head.append(script)
-    })
 }
 
-let promise = onLoad(
-  "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js"
-);
-
-promise.then(script => console.log(script.src), err => console.log(err));
-promise.then(res => console.log('another handler', res))
+loader("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js",ev=> {
+  loader(
+    "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js",
+    (ev) => {
+      loader(
+        "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js",
+        (ev) => {
+          document.write(ev.src)
+        }
+      );
+    });
+});
